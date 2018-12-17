cc50ccd14493f2ad5debee93d266f73263f89c83
Niels van Dijk <niels.vandijk@surfnet.nl>
2018-12-17 17:11:46 +0100

    Replaced run.sh with start.sh to prevent eccidental runnign of docker container

--- start.sh
+++ start.sh
@@ -3,10 +3,13 @@
 cd /tmp/inacademia/hinting
 
 # download eduGAN metadata to input dir
+echo "Downloading eduGAIN metadata"
 /usr/bin/wget http://mds.edugain.org/ -q  -O input/edugain.xml
+echo "Downloading eduGAIN metadata done"
 
 # pul the latest version of idp_hint data from the git repo
 cd output/idp_hint
+
 GIT_SSH_COMMAND='ssh -i /home/ubuntu/.ssh/id_rsa_inacademia' /usr/bin/git pull
 
 # parse the eduGAIN xml, this produces data in the output dir, potentially overwriting data in output/idp_hint
