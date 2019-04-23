88e92f64205b94af60982f083c4fa8b557cb94f3
Niels van Dijk <niels.vandijk@surfnet.nl>
2019-04-23 13:37:46 +0200

    Aded capability to create container if it is missing

--- run_hinting.sh
+++ run_hinting.sh
@@ -1,17 +1,24 @@
-
 #! /bin/bash
 IMAGE_TAG=inacademia/hinting:v1
+cd /home/ubuntu/inacademia-hinting
+
+# Build the docker image if needed
+if [[ "$(docker images -q $IMAGE_TAG 2> /dev/null)" == "" ]]; then
+  ./build_hinting.sh
+fi
+
 
 echo '##############################################################################################'
 echo $(date +"%c")
 echo '##############################################################################################'
 
 # Start SVS
-docker run -it \
-	-v /etc/passwd:/etc/passwd:ro \
-	-v /etc/group:/etc/group:ro \
-	-v /home/ubuntu:/home/ubuntu \
-	--mount src="$(pwd)/config",target=/tmp/config,type=bind \
-	--mount source=inacademia_admin_data,target=/tmp/inacademia_admin_data \
-	--hostname hinting.inacademia.local \
-	$IMAGE_TAG
+docker run -i \
+        -v /etc/passwd:/etc/passwd:ro \
+        -v /etc/group:/etc/group:ro \
+        -v /home/ubuntu:/home/ubuntu \
+        --mount src=/home/ubuntu/inacademia-hinting/config,target=/tmp/config,type=bind \
+        --mount source=inacademia_admin_data,target=/tmp/inacademia_admin_data \
+        --hostname hinting.inacademia.local \
+        $IMAGE_TAG
+
